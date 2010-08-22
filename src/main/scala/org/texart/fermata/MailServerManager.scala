dc4ad650d75604ecd1c442a5983f419f502cef5c
Greg Heartsfield <scsibug@imap.cc>
2010-08-22 18:04:00 -0500

    Added class to manage mail server connections.

---
+++ src/main/scala/org/texart/fermata/MailServerManager.scala
@@ -0,0 +1,50 @@
+package org.texart.fermata
+
+import scala.collection.mutable.HashMap
+
+import org.subethamail.smtp.server.SMTPServer
+import org.subethamail.smtp.MessageHandlerFactory
+import org.subethamail.smtp.MessageHandler
+import org.subethamail.smtp.RejectException
+import org.subethamail.smtp.MessageContext
+import java.io.IOException
+import java.io.InputStream
+
+
+class MailServerManager {
+  var serverMap = new HashMap[String, SMTPServer]
+ 
+  def startNewServer(name: String, port: Int) {
+    var server = new SMTPServer(new LoggingMessageHandlerFactory())
+    serverMap += name -> server
+    server.start()
+  }
+}
+
+class LoggingMessageHandlerFactory extends MessageHandlerFactory {
+  def create(ctx:MessageContext) = new Handler(ctx)
+}
+
+class Handler(ctx: MessageContext) extends MessageHandler {
+
+  @throws(classOf[RejectException])
+  def from(from: String) {
+    
+  }
+
+  @throws(classOf[RejectException])
+  def recipient(recipient: String) {
+    
+  }
+
+  @throws(classOf[IOException])
+  def data(data: InputStream) {
+
+  }
+  
+  def done {
+
+  }
+}
+
+
