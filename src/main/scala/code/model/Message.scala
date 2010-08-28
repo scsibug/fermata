998cd279261c6f5d5f398f39e3488edaff316bc6
Greg Heartsfield <scsibug@imap.cc>
2010-08-28 18:36:26 -0500

    Added Mapper entity class for mail messages.

---
+++ src/main/scala/code/model/Message.scala
@@ -0,0 +1,15 @@
+package code.model
+
+import _root_.net.liftweb.mapper._
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+
+class Message extends LongKeyedMapper[Message] with IdPK {
+  def getSingleton = Message
+  object msgBody extends MappedText(this)
+  object sender extends MappedEmail(this, 256)
+}
+
+object Message extends Message with LongKeyedMetaMapper[Message] {
+  override def fieldOrder = List(sender,msgBody)
+}
