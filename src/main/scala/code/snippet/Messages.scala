be55220fbfecfabe74aa27bd18572f1c32ea9beb
Greg Heartsfield <scsibug@imap.cc>
2010-08-29 11:11:33 -0500

    Index page displays sample data from new Messages snippet.

---
+++ src/main/scala/code/snippet/Messages.scala
@@ -0,0 +1,13 @@
+package code.snippet
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.java.util.Date
+import code.lib._
+import Helpers._
+
+class Messages {
+  def recentMessages(in: NodeSeq) : NodeSeq =
+    <ul><li>foo</li><li>bar</li></ul>
+}
