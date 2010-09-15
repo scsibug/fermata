8e6719b9a205c0f5dccee0a7121cf4272125d8dc
Greg Heartsfield <scsibug@imap.cc>
2010-09-15 18:48:04 -0500

    Indexing id/textcontent of new messages.

--- src/main/scala/org/texart/fermata/MessageIndex.scala
+++ src/main/scala/org/texart/fermata/MessageIndex.scala
@@ -3,6 +3,7 @@ import org.apache.lucene.index.{IndexWriter}
 import org.apache.lucene.store.{SimpleFSDirectory}
 import org.apache.lucene.analysis.standard.{StandardAnalyzer}
 import org.apache.lucene.util.Version.{LUCENE_30}
+import org.apache.lucene.document.{Document,Field}
 import net.liftweb.actor._
 import code.comet.{NewMessage}
 import code.model.Message
@@ -21,6 +22,23 @@ object MessageIndex extends LiftActor {
     case NewMessage(msg: Message) => {
       println("got new msg")
     }
+  }
+
+  def indexMessage(msg: Message) = {
+    val doc : Document = new Document()
+    val idField : Field =
+      new Field("id",
+                msg.id.toString,
+                Field.Store.YES,
+                Field.Index.NO)
+    val textContentField : Field =
+      new Field("textcontent",
+                msg.textContent,
+                Field.Store.YES,
+                Field.Index.ANALYZED)
+    doc.add(idField)
+    doc.add(textContentField)
+    index addDocument doc
   }
 
 }
