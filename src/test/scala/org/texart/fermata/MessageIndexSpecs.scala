28cd300d506ec6d9f8d4a0e4458b534a9775fdb9
Dhruv Chandna <dhruvchandna@gmail.com>
2010-09-15 20:59:13 -0500

    added test case for message index search.

---
+++ src/test/scala/org/texart/fermata/MessageIndexSpecs.scala
@@ -0,0 +1,26 @@
+package org.texart.fermata
+
+import net.liftweb.mapper.{Schemifier}
+import net.liftweb.util.{Log}
+
+import code.util.DBUtil
+
+import org.specs.Specification
+import org.specs.runner.JUnit4
+import code.model.{Message}
+class MessageIndexSpecsAsTest extends JUnit4(MessageIndexSpecs)
+
+object MessageIndexSpecs extends Specification{
+	    "MessageIndex" should {
+        doFirst {
+            DBUtil.initialize
+            Schemifier.schemify(true, Schemifier.infoF _, Message)
+            DBUtil.setupDB("dbunit/simple_message.xml")
+			
+        }
+		"search the index for message" in {
+			var results = MessageIndex search "content"
+			(results.length == 1) must beTrue
+		}
+	}	
+}
