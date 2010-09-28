f7ac01f76f7f1b3c2d8fea63c2a82fab173cacb8
Greg Heartsfield <scsibug@imap.cc>
2010-09-27 22:59:14 -0500

    Dependency changed to official 2.1 release of Lift, not snapshot.

--- project/build/FermataProject.scala
+++ project/build/FermataProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class FermataProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.1-SNAPSHOT"
+  val liftVersion = "2.1"
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots/"
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
