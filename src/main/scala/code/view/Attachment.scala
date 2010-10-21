ae2cad518907b008d216f8008c7e3264ce024f8b
Greg Heartsfield <scsibug@imap.cc>
2010-10-21 02:56:02 -0500

    Added Attachment view.

---
+++ src/main/scala/code/view/Attachment.scala
@@ -0,0 +1,30 @@
+package code.view
+
+import net.liftweb.common.{Box,Empty,Full}
+import net.liftweb.http.{ForbiddenResponse,InMemoryResponse,LiftResponse,OutputStreamResponse}
+import java.io.{OutputStream,ByteArrayOutputStream}
+import net.liftweb.common.Logger
+import code.model.Message
+import org.apache.commons.io.{IOUtils}
+
+object Attachment extends Logger {
+  def view (msgid: String, attId: String): Box[LiftResponse] = {
+    info("Request to view msg "+msgid+", attachment #"+attId)
+    val message : Box[Message] = Message.getMessageById(msgid.toLong)
+    val response = message match {
+      case Full(msg) => {
+        val (contentType, attachmentStream) = msg.getAttachment(attId.toInt)
+        val os :OutputStream = new ByteArrayOutputStream()
+        def writer(os:OutputStream): Unit = {
+          IOUtils.copy(attachmentStream,os)
+          os.close()
+        }
+        val osr = OutputStreamResponse(writer _, List(Pair("Content-Type", contentType)))
+        Full(osr)
+      }
+      case _ => Empty
+    }
+    response
+  }
+}
+
