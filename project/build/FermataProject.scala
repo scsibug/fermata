cfcd9dbf0ff5c13d07f0b501b02c64f156473dcf
Greg Heartsfield <scsibug@imap.cc>
2012-12-05 18:46:11 -0600

    Update dependencies and repos.

--- project/build/FermataProject.scala
+++ project/build/FermataProject.scala
@@ -1,17 +1,18 @@
 import sbt._
-import net.usersource.jettyembed.JettyEmbedWebProject
+import net.usersource.jettyembed._
 
 class FermataProject(info: ProjectInfo) extends JettyEmbedWebProject(info) {
   val liftVersion = "2.1"
+  val stScalaToolsReleases = "Sonatype Scala-Tools Maven2 Snapshots Repository" at "https://oss.sonatype.org/content/groups/scala-tools/"
+  val stScalaToolsSnapshots = "Sonatype Scala-Tools Maven2 Snapshots Repository" at "https://oss.sonatype.org/content/repositories/snapshots/"
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots/"
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
     "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default",
     "junit" % "junit" % "4.7" % "test->default",
-    "org.scala-tools.testing" % "specs_2.8.0.RC7" % "1.6.5-SNAPSHOT" % "test->default",
     "org.dbunit" % "dbunit" % "2.4.8" % "test->default",
     "com.h2database" % "h2" % "1.2.138",
     "org.subethamail" % "subethasmtp" % "3.1.4",
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
- val jettyEmbeddedWar = "net.usersource" % "jetty-embed-plugin" % "0.2-SNAPSHOT"
+  val jettyEmbeddedWarRepo = "Embeded Jetty Repo" at "https://github.com/glenford/repo/raw/master"
+  val jettyEmbeddedWar = "net.usersource" % "sbt-jetty-embed-plugin" % "0.6.1"
 }
